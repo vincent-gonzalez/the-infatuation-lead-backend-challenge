d354c3892edd1cac866d2be6cb82622c854e0364
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-20 15:24:33 -0400

    Clean up commented code

--- src/main.go
+++ src/main.go
@@ -3,52 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
-	//"os"
-	//"io"
 	"net"
 	"strings"
 	"strconv"
-	// "time"
 	"sort"
 )
 
 func main() {
 	fmt.Println("Hello World!");
-	// fmt.Println("Starting server...");
-	// server, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9090));
-	// if err != nil {
-	// 	fmt.Println("Error starting server:", err.Error());
-	// 	os.Exit(1);
-	// }
-	// fmt.Println("Server started.");
-	// defer server.Close();
-	// fmt.Println("Waiting for client...");
-	// isAllEventsReceived := false;
-	// for !isAllEventsReceived {
-	// 	connection, err := server.Accept();
-	// 	if err != nil {
-	// 		fmt.Println("Error in client connection:", err.Error());
-	// 		os.Exit(1);
-	// 	}
-	// 	fmt.Println("client connected");
-	// 	// do go routine here
-	// 	buffer := make([]byte, 1024);
-	// 	messageLength, err := connection.Read(buffer);
-	// 	if err != nil {
-	// 		fmt.Println("Error reading:", err.Error());
-	// 	}
-	// 	message := string(buffer[:messageLength]);
-	// 	fmt.Println("Message received: ", message);
-	// 	if message == "EVENT END" {
-	// 		isAllEventsReceived = true;
-	// 		connection.Close();
-	// 	}
-	// }
+
 	connection, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", 9090));
 	if err != nil {
 		fmt.Println("Error connecting to EVENT SOURCE:", err.Error());
 	}
-	defer connection.Close()
+	defer connection.Close();
 	reader := bufio.NewReader(connection);
 	fmt.Println("Waiting for server...");
 	isAllEventsReceived := false;
@@ -56,24 +24,29 @@ func main() {
 	likeMatches := make(map[string][]LikeEvent);
 	var matchSequenceNumbers []uint64;
 	for !isAllEventsReceived {
-		//buffer := make([]byte, 1024);
-		//messageLength, err := connection.Read(buffer);
 		message, err := reader.ReadString('\n');
 		if err != nil {
 			fmt.Println("Error reading:", err.Error());
 		}
-		//message := string(buffer[:messageLength]);
 		message = message[:len(message)-1]
-		//fmt.Println(message);
-		//fmt.Println(message == "EVENT BEGIN");
-		//time.Sleep(2 * time.Second);
+
 		if message == "EVENT BEGIN" {
 			continue;
 		} else if message == "EVENT END" {
 			isAllEventsReceived = true;
 		} else {
-			//message = message[:len(message)-1]
 			messageParts := strings.Split(message, "|");
+			// sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
+			// 	if err != nil {
+			// 		fmt.Println("Sequence Num not number: ", err.Error());
+			// 	}
+			// 	newLikeEvent := LikeEvent{
+			// 		SequenceNum: sequenceNum,
+			// 		LikeType: messageParts[1],
+			// 		FromUserId: messageParts[2],
+			// 		ToUserId: messageParts[3],
+			// 	};
+			// 	likeEvents = append(likeEvents, newLikeEvent);
 			if messageParts[1] == "LIKE_LIKED" {
 				sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
 				if err != nil {
@@ -89,44 +62,30 @@ func main() {
 				if events, found := likeMatches[newLikeEvent.ToUserId]; found {
 					var matchingEvent LikeEvent;
 					var matchSequenceNum uint64;
+					foundMatchingEvent := false;
 					for _, m := range events {
 						if m.ToUserId == newLikeEvent.FromUserId {
 							matchingEvent = m;
+							foundMatchingEvent = true;
 							break;
 						}
 					}
-					if matchingEvent.SequenceNum > newLikeEvent.SequenceNum {
-						matchSequenceNum = matchingEvent.SequenceNum;
-					} else {
-						matchSequenceNum = newLikeEvent.SequenceNum;
-					}
+					if foundMatchingEvent {
+						if matchingEvent.SequenceNum > newLikeEvent.SequenceNum {
+							matchSequenceNum = matchingEvent.SequenceNum;
+						} else {
+							matchSequenceNum = newLikeEvent.SequenceNum;
+						}
 
-					matchSequenceNumbers = append(matchSequenceNumbers, matchSequenceNum);
+						matchSequenceNumbers = append(matchSequenceNumbers, matchSequenceNum);
+					}
 				} else {
 					likeMatches[newLikeEvent.FromUserId] = append(likeMatches[newLikeEvent.FromUserId], newLikeEvent);
 				}
 			}
 		}
-		// message = message[:len(message)-1]
-		// messageParts := strings.Split(message, "|");
-		// sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
-		// if err != nil {
-		// 	fmt.Println("Sequence Num not number: ", err.Error());
-		// }
-		// likeEvents = append(likeEvents, LikeEvent{
-		// 	SequenceNum: sequenceNum,
-		// 	LikeType: messageParts[1],
-		// 	FromUserId: messageParts[2],
-		// 	ToUserId: messageParts[3],
-		// });
-		//fmt.Println("Message received: ", message);
-		// if message == "EVENT END" {
-		// 	isAllEventsReceived = true;
-		// 	//connection.Close();
-		// 	//break;
-		// }
 	}
-	//connection.Close();
+
 	fmt.Println("All messages received.");
 	fmt.Println("Connection closed.");
 	fmt.Println("Server shutdown.");
