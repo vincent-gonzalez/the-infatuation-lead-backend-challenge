c7be257b2802c0703a7882daf9db76ed7b173d3a
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-20 14:58:05 -0400

    WIP

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	"net"
 	"strings"
 	"strconv"
+	// "time"
+	"sort"
 )
 
 func main() {
@@ -51,6 +53,8 @@ func main() {
 	fmt.Println("Waiting for server...");
 	isAllEventsReceived := false;
 	var likeEvents []LikeEvent;
+	likeMatches := make(map[string][]LikeEvent);
+	var matchSequenceNumbers []uint64;
 	for !isAllEventsReceived {
 		//buffer := make([]byte, 1024);
 		//messageLength, err := connection.Read(buffer);
@@ -60,30 +64,83 @@ func main() {
 		}
 		//message := string(buffer[:messageLength]);
 		message = message[:len(message)-1]
-		messageParts := strings.Split(message, "|");
-		sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
-		if err != nil {
-			fmt.Println("Sequence Num not number: ", err.Error());
-		}
-		likeEvents = append(likeEvents, LikeEvent{
-			SequenceNum: sequenceNum,
-			LikeType: messageParts[1],
-			FromUserId: messageParts[2],
-			ToUserId: messageParts[3],
-		});
-		//fmt.Println("Message received: ", message);
-		if message == "EVENT END" {
+		//fmt.Println(message);
+		//fmt.Println(message == "EVENT BEGIN");
+		//time.Sleep(2 * time.Second);
+		if message == "EVENT BEGIN" {
+			continue;
+		} else if message == "EVENT END" {
 			isAllEventsReceived = true;
-			//connection.Close();
-			//break;
+		} else {
+			//message = message[:len(message)-1]
+			messageParts := strings.Split(message, "|");
+			if messageParts[1] == "LIKE_LIKED" {
+				sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
+				if err != nil {
+					fmt.Println("Sequence Num not number: ", err.Error());
+				}
+				newLikeEvent := LikeEvent{
+					SequenceNum: sequenceNum,
+					LikeType: messageParts[1],
+					FromUserId: messageParts[2],
+					ToUserId: messageParts[3],
+				};
+				likeEvents = append(likeEvents, newLikeEvent);
+				if events, found := likeMatches[newLikeEvent.ToUserId]; found {
+					var matchingEvent LikeEvent;
+					var matchSequenceNum uint64;
+					for _, m := range events {
+						if m.ToUserId == newLikeEvent.FromUserId {
+							matchingEvent = m;
+							break;
+						}
+					}
+					if matchingEvent.SequenceNum > newLikeEvent.SequenceNum {
+						matchSequenceNum = matchingEvent.SequenceNum;
+					} else {
+						matchSequenceNum = newLikeEvent.SequenceNum;
+					}
+
+					matchSequenceNumbers = append(matchSequenceNumbers, matchSequenceNum);
+				} else {
+					likeMatches[newLikeEvent.FromUserId] = append(likeMatches[newLikeEvent.FromUserId], newLikeEvent);
+				}
+			}
 		}
+		// message = message[:len(message)-1]
+		// messageParts := strings.Split(message, "|");
+		// sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64);
+		// if err != nil {
+		// 	fmt.Println("Sequence Num not number: ", err.Error());
+		// }
+		// likeEvents = append(likeEvents, LikeEvent{
+		// 	SequenceNum: sequenceNum,
+		// 	LikeType: messageParts[1],
+		// 	FromUserId: messageParts[2],
+		// 	ToUserId: messageParts[3],
+		// });
+		//fmt.Println("Message received: ", message);
+		// if message == "EVENT END" {
+		// 	isAllEventsReceived = true;
+		// 	//connection.Close();
+		// 	//break;
+		// }
 	}
 	//connection.Close();
 	fmt.Println("All messages received.");
 	fmt.Println("Connection closed.");
 	fmt.Println("Server shutdown.");
 	fmt.Println("Printing received messages...");
+	sort.Slice(likeEvents, func(i, j int) bool {
+		return likeEvents[i].SequenceNum < likeEvents[j].SequenceNum;
+	});
+	sort.Slice(matchSequenceNumbers, func (i, j int) bool {
+		return matchSequenceNumbers[i] < matchSequenceNumbers[j];
+	});
 	for _, event := range likeEvents {
-		fmt.Printf("Sequence Num: %d Like Type: %s From User: %s To User: %s", event.SequenceNum, event.LikeType, event.FromUserId, event.ToUserId);
+		fmt.Printf("Sequence Num: %d Like Type: %s From User: %s To User: %s\n", event.SequenceNum, event.LikeType, event.FromUserId, event.ToUserId);
+	}
+	for _, s := range matchSequenceNumbers {
+		fmt.Printf("Match num: %d\n", s);
 	}
 }
