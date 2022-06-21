1e2b78b8eb5d8c751831285c8f1bc166100c0823
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-21 16:38:42 -0400

    Add comments. Remove commented code

--- src/main.go
+++ src/main.go
@@ -12,11 +12,16 @@ func main() {
 	likeEvents, err := ReceiveEvents("tcp", "localhost", 9090)
 	if err != nil {
 		// exit here. we can't process further without a list of valid events
+		// TODO - we could make a new request to the server to attempt to get
+		// the events again.
 		fmt.Printf("Failed while receive events: %v\n", err.Error())
 		os.Exit(1)
 	}
 
 	fmt.Println("Sorting events...");
+	// TODO - built in sorting is okay for simple data sets, but we may want
+	// to upgrade to a better algorithm if the data becomes larger or more
+	// complex.
 	sort.Slice(likeEvents, func(i, j int) bool {
 		return likeEvents[i].SequenceNum < likeEvents[j].SequenceNum
 	});
@@ -27,19 +32,9 @@ func main() {
 		fmt.Printf("Failed while finding match events: %v\n", err.Error())
 	}
 
-	// fmt.Println("Sorting sequence numbers...");
-	// sort.Slice(matchSequenceNumbers, func (i, j int) bool {
-	// 	return matchSequenceNumbers[i] < matchSequenceNumbers[j]
-	// });
-
 	if len(matchSequenceNumbers) < 1 {
 		fmt.Println("No matches found.")
 	} else {
-		// fmt.Println("Sorting sequence numbers...");
-		// sort.Slice(matchSequenceNumbers, func (i, j int) bool {
-		// 	return matchSequenceNumbers[i] < matchSequenceNumbers[j]
-		// });
-
 		err := SendMatchEvents(matchSequenceNumbers, "tcp", "localhost", 9099)
 		if err != nil {
 			fmt.Printf("Failed while sending match events: %v", err.Error())
