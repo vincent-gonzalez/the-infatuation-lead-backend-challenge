ca13581b44cb6078827f0007e3d1345052b7345e
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-21 18:53:10 -0400

    Remove commented code

--- src/EventFunctions.go
+++ src/EventFunctions.go
@@ -53,54 +53,6 @@ func ReceiveEvents(protocol string, eventSourceURL string, port uint) ([]LikeEve
 	return likeEvents, nil
 }
 
-// func ParseEvents(scanner *bufio.Scanner, dataDelimiter string) ([]LikeEvent, error) {
-// 	var likeEvents []LikeEvent
-
-// 	// wait for EVENT SOURCE to send start message
-// 	for scanner.Scan() && scanner.Text() != "EVENT BEGIN" {
-// 		fmt.Println("Waiting for EVENT SOURCE to send events...")
-// 	}
-// 	fmt.Println("EVENT BEGIN")
-
-// 	for scanner.Scan() {
-// 		message := scanner.Text()
-
-// 		if message == "EVENT END" {
-// 			fmt.Println(message)
-// 			break
-// 		} else {
-// 			// messageParts := strings.Split(message, dataDelimiter)
-// 			// sequenceNum, err := strconv.ParseUint(messageParts[0], 10, 64)
-// 			// if err != nil {
-// 			// 	fmt.Println("Input sequence number is not a number: ", err.Error())
-// 			// 	// if sequence number is malformed, we can't parse it correctly.
-// 			// 	// we can't eventually find matches without a sequence number.
-// 			// 	// in a simple case, dropping the record is fine, but in a real
-// 			// 	// world scenario, this may not be acceptable and should be handled.
-// 			// 	continue
-// 			// }
-
-// 			// newLikeEvent := LikeEvent{
-// 			// 	SequenceNum: sequenceNum,
-// 			// 	LikeType:    messageParts[1],
-// 			// 	FromUserId:  messageParts[2],
-// 			// 	ToUserId:    messageParts[3],
-// 			// }
-// 			newLikeEvent, err := ParseEvent(message, dataDelimiter)
-// 			if err != nil {
-// 				fmt.Println("Failed to parse event message.")
-// 				// in a simple case, dropping the record is fine, but in a real
-// 				// world scenario, this may not be acceptable and should be handled.
-// 				continue
-// 			}
-
-// 			likeEvents = append(likeEvents, *newLikeEvent)
-// 		}
-// 	}
-
-// 	return likeEvents, nil
-// }
-
 func ParseEvent(eventMessage, dataDelimiter string, numberOfFields int) (*LikeEvent, error) {
 	messageParts := strings.Split(eventMessage, dataDelimiter)
 	if len(messageParts) < numberOfFields {
