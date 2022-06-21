8617907b36e492969a8bc1f4a11b51524df3f89e
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-21 18:53:34 -0400

    Add parse test and no events test

--- src/EventFunctions_test.go
+++ src/EventFunctions_test.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"testing"
 )
+
+func TestParseEvent(t *testing.T) {
+	testEvent := "5|LIKE_LIKED|300|100"
+
+	got, _ := ParseEvent(testEvent, "|", 4)
+	want := LikeEvent{
+		SequenceNum: 5,
+		LikeType: "LIKE_LIKED",
+		FromUserId: "300",
+		ToUserId: "100",
+	}
+
+	if *got != want {
+		t.Errorf("Expected %v but got %v", want, got)
+	}
+}
 
 func TestFindMatchEvents(t *testing.T) {
 	testMatches := []LikeEvent {
@@ -56,8 +72,63 @@ func TestFindMatchEvents(t *testing.T) {
 
 	if !testSlicesAreEqual(want, got) {
 		t.Errorf("Expected %v, but got %v\n", want, got);
-	} else {
-		fmt.Printf("Expected %v and got %v\n", want, got);
+	}
+	//  else {
+	// 	fmt.Printf("Expected %v and got %v\n", want, got);
+	// }
+}
+
+func TestNoMatchEventsFound(t *testing.T) {
+	matches := []LikeEvent {
+		{
+			SequenceNum: 1,
+			LikeType: "LIKE_UNSPECIFIED",
+			FromUserId: "300",
+			ToUserId: "100",
+		},
+		{
+			SequenceNum: 2,
+			LikeType: "LIKE_NOT_LIKED",
+			FromUserId: "100",
+			ToUserId: "200",
+		},
+		{
+			SequenceNum: 3,
+			LikeType: "LIKE_NOT_LIKED",
+			FromUserId: "300",
+			ToUserId: "200",
+		},
+		{
+			SequenceNum: 4,
+			LikeType: "LIKE_LIKED",
+			FromUserId: "200",
+			ToUserId: "100",
+		},
+		{
+			SequenceNum: 5,
+			LikeType: "LIKE_NOT_LIKED",
+			FromUserId: "300",
+			ToUserId: "100",
+		},
+		{
+			SequenceNum: 6,
+			LikeType: "LIKE_UNSPECIFIED",
+			FromUserId: "200",
+			ToUserId: "300",
+		},
+		{
+			SequenceNum: 7,
+			LikeType: "LIKE_LIKED",
+			FromUserId: "100",
+			ToUserId: "300",
+		},
+	}
+
+	got, _ := FindMatchEvents(matches)
+	// want := []uint{}
+
+	if len(got) != 0 {
+		t.Errorf("Expected length of matches to be zero. Got: %d", len(got))
 	}
 }
 
