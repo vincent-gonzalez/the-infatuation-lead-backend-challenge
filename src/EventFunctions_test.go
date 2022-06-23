4b155a2cfa9833dc9bc44d77c5d764b6b3f709ab
Vincent Gonzalez <vincentmpgonzalez@gmail.com>
2022-06-23 01:36:40 -0400

    Format code properly

--- src/EventFunctions_test.go
+++ src/EventFunctions_test.go
@@ -11,17 +11,18 @@ func TestParseEvent(t *testing.T) {
 	got, _ := ParseEvent(testEvent, "|", 4)
 	want := LikeEvent{
 		SequenceNum: 5,
-		LikeType: "LIKE_LIKED",
-		FromUserId: "300",
-		ToUserId: "100",
+		LikeType:    "LIKE_LIKED",
+		FromUserId:  "300",
+		ToUserId:    "100",
 	}
 
 	if *got != want {
 		t.Errorf("Expected %v but got %v", want, got)
 	}
 }
+
 // Test that a malformed message is not parsed.
-func TestParseEventBadEvent (t *testing.T) {
+func TestParseEventBadEvent(t *testing.T) {
 	testEvent := "5,LIKE_LIKED,300,100"
 
 	got, err := ParseEvent(testEvent, "|", 4)
@@ -33,103 +34,103 @@ func TestParseEventBadEvent (t *testing.T) {
 
 // Test that matches are found with known good input that contains match events.
 func TestFindMatchEvents(t *testing.T) {
-	testMatches := []LikeEvent {
+	testMatches := []LikeEvent{
 		{
 			SequenceNum: 1,
-			LikeType: "LIKE_UNSPECIFIED",
-			FromUserId: "300",
-			ToUserId: "100",
+			LikeType:    "LIKE_UNSPECIFIED",
+			FromUserId:  "300",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 2,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "100",
-			ToUserId: "200",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "100",
+			ToUserId:    "200",
 		},
 		{
 			SequenceNum: 3,
-			LikeType: "LIKE_NOT_LIKED",
-			FromUserId: "300",
-			ToUserId: "200",
+			LikeType:    "LIKE_NOT_LIKED",
+			FromUserId:  "300",
+			ToUserId:    "200",
 		},
 		{
 			SequenceNum: 4,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "200",
-			ToUserId: "100",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "200",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 5,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "300",
-			ToUserId: "100",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "300",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 6,
-			LikeType: "LIKE_UNSPECIFIED",
-			FromUserId: "200",
-			ToUserId: "300",
+			LikeType:    "LIKE_UNSPECIFIED",
+			FromUserId:  "200",
+			ToUserId:    "300",
 		},
 		{
 			SequenceNum: 7,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "100",
-			ToUserId: "300",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "100",
+			ToUserId:    "300",
 		},
 	}
 
-	got, _ := FindMatchEvents(testMatches);
-	want := []uint64 {4, 7};
+	got, _ := FindMatchEvents(testMatches)
+	want := []uint64{4, 7}
 
 	if !testSlicesAreEqual(want, got) {
-		t.Errorf("Expected %v, but got %v\n", want, got);
+		t.Errorf("Expected %v, but got %v\n", want, got)
 	}
 }
 
 // Test that match events are not found when the input is known to not have match events.
 func TestNoMatchEventsFound(t *testing.T) {
-	matches := []LikeEvent {
+	matches := []LikeEvent{
 		{
 			SequenceNum: 1,
-			LikeType: "LIKE_UNSPECIFIED",
-			FromUserId: "300",
-			ToUserId: "100",
+			LikeType:    "LIKE_UNSPECIFIED",
+			FromUserId:  "300",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 2,
-			LikeType: "LIKE_NOT_LIKED",
-			FromUserId: "100",
-			ToUserId: "200",
+			LikeType:    "LIKE_NOT_LIKED",
+			FromUserId:  "100",
+			ToUserId:    "200",
 		},
 		{
 			SequenceNum: 3,
-			LikeType: "LIKE_NOT_LIKED",
-			FromUserId: "300",
-			ToUserId: "200",
+			LikeType:    "LIKE_NOT_LIKED",
+			FromUserId:  "300",
+			ToUserId:    "200",
 		},
 		{
 			SequenceNum: 4,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "200",
-			ToUserId: "100",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "200",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 5,
-			LikeType: "LIKE_NOT_LIKED",
-			FromUserId: "300",
-			ToUserId: "100",
+			LikeType:    "LIKE_NOT_LIKED",
+			FromUserId:  "300",
+			ToUserId:    "100",
 		},
 		{
 			SequenceNum: 6,
-			LikeType: "LIKE_UNSPECIFIED",
-			FromUserId: "200",
-			ToUserId: "300",
+			LikeType:    "LIKE_UNSPECIFIED",
+			FromUserId:  "200",
+			ToUserId:    "300",
 		},
 		{
 			SequenceNum: 7,
-			LikeType: "LIKE_LIKED",
-			FromUserId: "100",
-			ToUserId: "300",
+			LikeType:    "LIKE_LIKED",
+			FromUserId:  "100",
+			ToUserId:    "300",
 		},
 	}
 
@@ -143,14 +144,14 @@ func TestNoMatchEventsFound(t *testing.T) {
 // Helper function for uint64 slices.
 func testSlicesAreEqual(a, b []uint64) bool {
 	if len(a) != len(b) {
-		return false;
+		return false
 	}
 
 	for i, v := range a {
 		if v != b[i] {
-			return false;
+			return false
 		}
 	}
 
-	return true;
+	return true
 }
